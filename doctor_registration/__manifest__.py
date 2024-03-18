f2ca5b1b300ec34d1d6272fa376af8165c58d961
Zafir Abdullah <63918801+Zafir547@users.noreply.github.com>
2024-03-18 06:42:32 +0500

    Doctor_Registration

---
+++ doctor_registration/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Doctor Registration",
+    'version': '1.0',
+    'sequence': -100,
+    'author': "Zafir Abdullah",
+    'category': 'Doctor',
+    'summary': 'Docter Registration system',
+    'description': 'Docter Registration system',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/doctor_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ doctor_registration/models/doctors.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class HospitalDoctor(models.Model):
+    _name = 'hospital.doctor'
+    _description = 'Doctor Information'
+    
+    name = fields.Char(string='Doctor Name')
+    cnic = fields.Char(string='CNIC')
+    mobile_no = fields.Char(string='Mobile No')
+    email = fields.Char(string='Email')
