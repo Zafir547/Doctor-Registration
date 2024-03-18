0fd5ab15877fab592a60d835744ed305f190edc0
Zafir Abdullah <63918801+Zafir547@users.noreply.github.com>
2024-03-18 17:37:42 +0500

    Update Doctor Registration

--- doctor_registration/models/doctors.py
+++ doctor_registration/models/doctors.py
@@ -3,8 +3,8 @@ from odoo import models, fields
 
 class HospitalDoctor(models.Model):
     _name = 'hospital.doctor'
-    _description = 'Hospital Doctor'
-    
+    _description = 'Doctor Information'
+
     name = fields.Char(string='Doctor Name')
     cnic = fields.Char(string='CNIC')
     mobile_no = fields.Char(string='Mobile No')
